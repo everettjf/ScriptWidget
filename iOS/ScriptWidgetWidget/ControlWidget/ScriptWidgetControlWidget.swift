84c03c151171e0cdd8351e30b6d1388bbab9b877
everettjf <everettjf@live.com>
2024-11-05 22:52:30 -0800

    timer test

--- iOS/ScriptWidgetWidget/ControlWidget/ScriptWidgetControlWidget.swift
+++ iOS/ScriptWidgetWidget/ControlWidget/ScriptWidgetControlWidget.swift
@@ -8,10 +8,28 @@ import WidgetKit
 
 struct ScriptWidgetControlWidget: ControlWidget {
   var body: some ControlWidgetConfiguration {
-    StaticControlConfiguration(kind: "ScriptWidget") {
-      ControlWidgetButton(action: ScriptWidgetControlAppIntent()) {
-        Label("Click Script Widget", systemImage: "gamecontroller")
+//    StaticControlConfiguration(kind: "ScriptWidget") {
+//      ControlWidgetButton(action: ScriptWidgetControlAppIntent()) {
+//        Label("Click Script Widget", systemImage: "gamecontroller")
+//      }
+//    }
+      
+
+      AppIntentControlConfiguration(
+        kind: "ScriptWidget",
+        provider: ConfigurableProvider()
+      ) { timerState in
+        ControlWidgetToggle(
+          timerState.timer.name,
+          isOn: timerState.isRunning,
+          action: ToggleTimerIntent(timer: timerState.timer),
+          valueLabel: { isOn in
+            Label(isOn ? "Running" : "Stopped", systemImage: "timer")
+          }
+        )
       }
-    }
+      .displayName("Productivity Timer")
+      .description("Start and stop a productivity timer.")
+      .promptsForUserConfiguration()
   }
 }
