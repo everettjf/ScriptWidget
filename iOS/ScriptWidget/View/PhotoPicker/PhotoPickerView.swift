cde43976bef629f3bdc5bd2f8320a54ab862fd76
everettjf <everettjf@live.com>
2026-02-21 11:52:15 -0800

    fix crash

--- iOS/ScriptWidget/View/PhotoPicker/PhotoPickerView.swift
+++ iOS/ScriptWidget/View/PhotoPicker/PhotoPickerView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UIKit
 
 
 struct PhotoPickerView: UIViewControllerRepresentable {
@@ -15,22 +16,48 @@ struct PhotoPickerView: UIViewControllerRepresentable {
     
     @Environment(\.presentationMode) var presentationMode
 
-    typealias UIViewControllerType = PhotoPickerViewController
+    typealias UIViewControllerType = UIViewController
     
-    func makeUIViewController(context: Context) -> PhotoPickerViewController {
-        let storyboard = UIStoryboard(name: "PhotoPicker", bundle: nil)
-        let vc = storyboard.instantiateViewController(identifier: "PhotoPickerViewController") as! PhotoPickerViewController
+    func makeUIViewController(context: Context) -> UIViewController {
+        let bundle = Bundle.main
+
+        // Avoid a hard crash if the storyboard is accidentally missing from a build.
+        guard bundle.path(forResource: "PhotoPicker", ofType: "storyboardc") != nil else {
+            let vc = UIViewController()
+            vc.view.backgroundColor = .systemBackground
+
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.text = "Photo picker is unavailable in this build."
+            label.textAlignment = .center
+            label.numberOfLines = 0
+            label.textColor = .secondaryLabel
+
+            vc.view.addSubview(label)
+            NSLayoutConstraint.activate([
+                label.leadingAnchor.constraint(equalTo: vc.view.leadingAnchor, constant: 24),
+                label.trailingAnchor.constraint(equalTo: vc.view.trailingAnchor, constant: -24),
+                label.centerYAnchor.constraint(equalTo: vc.view.centerYAnchor),
+            ])
+            return vc
+        }
+
+        let storyboard = UIStoryboard(name: "PhotoPicker", bundle: bundle)
+        let viewController = storyboard.instantiateViewController(identifier: "PhotoPickerViewController")
+        guard let vc = viewController as? PhotoPickerViewController else {
+            return UIViewController()
+        }
+
         vc.popAction = {
             self.presentationMode.wrappedValue.dismiss()
         }
-        // init
         vc.scriptModel = self.scriptModel
         return vc
     }
     
-    func updateUIViewController(_ vc: PhotoPickerViewController, context: Context) {
-        // update
-        vc.scriptModel = self.scriptModel
+    func updateUIViewController(_ vc: UIViewController, context: Context) {
+        guard let picker = vc as? PhotoPickerViewController else { return }
+        picker.scriptModel = self.scriptModel
     }
     
     func makeCoordinator() -> Coordinator {
